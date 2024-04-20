735253bd2f83b76a8cadeda47ea6b39bcbdd741b
qiyudada <109809108+qiyudada@users.noreply.github.com>
2024-04-20 09:53:52 +0800

    Add files via upload

---
+++ System/Delay.h
@@ -0,0 +1,20 @@
+#ifndef __DELAY_H
+#define __DELAY_H 			   
+#include "sys.h"
+
+//void Delay_init(void);
+//void Delay_ms(u16 nms);
+//void Delay_us(u32 nus);
+
+void Delay_us(uint32_t xus);
+void Delay_ms(uint32_t xms);
+void Delay_s(uint32_t xs);
+
+#endif
+
+
+
+
+
+
+
